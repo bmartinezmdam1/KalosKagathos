e11e38490b18ccd74abf912ac9dd9fe8db4c065b
bmartinezmdam1 <bmartinezmdam1@gmail.com>
2024-05-24 13:30:51 +0200

    intento de navegación con firebase (fallido))

--- app/src/main/java/com/example/fitnesscoach/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/fitnesscoach/activities/RegistroActivity.kt
@@ -22,6 +22,8 @@ class RegistroActivity : Activity(){
     private lateinit var editTextEmail: EditText
     private lateinit var editTextContrasena: EditText
     private lateinit var editTextConfirmarContrasena: EditText
+    private lateinit var name:String
+    private lateinit var contrasena:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,24 +43,25 @@ class RegistroActivity : Activity(){
         if (!isEmailValid()) return
 
         if (!isPasswordValid()) return
-        // Guardar datos de registo en Firebase
-        //    try {
-        //      db.collection("usuarios").document(editTextEmail.text.toString()).set() {
-        //          hashMapOf(
-        //             "nombre" to editTextNombre.text.toString(),
-        //         "contrasena" to editTextContrasena.text.toString()
-        //       )
-        //      }
-        //     }
-        //   catch (e: Exception) {
-        //    Log.e("Error", "Error al añadir usuario: $e")
-        //
-        try {
-            startActivity(Intent(this, LoginActivity::class.java))
+        CoroutineScope(Dispatchers.Main).launch {
+          try {
+            db.collection("usuarios").document(editTextEmail.text.toString()).set() {
+               hashMapOf(
+                     "nombre" to editTextNombre.text.toString(),
+                "contrasena" to editTextContrasena.text.toString()
+              )
+            }
+            }
+          catch (e: Exception) {
+           Log.e("Error", "Error al añadir usuario: $e")
+          }
         }
-            catch (e: Exception) {
-                e.printStackTrace()
-         }
+        //  try {
+        //      startActivity(Intent(this, LoginActivity::class.java))
+        //  }
+        //    catch (e: Exception) {
+        //       e.printStackTrace()
+        // }
 
         // TODO: Redirigir a login (segunda versión que envíe el email y pass al login e inicie sesión automáticamente)
 
@@ -80,10 +83,33 @@ class RegistroActivity : Activity(){
 
 
     private fun isUsernameValid(): Boolean {
+       var email = editTextEmail.text.toString();
         val username = editTextNombre.text.toString()
+        val stringList: ArrayList<String> = ArrayList()
 
         if (username.length !in 6..20) {
             Toast.makeText(this, "El nombre de usuario debe tener entre 6 y 20 caracteres", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        CoroutineScope(Dispatchers.Main).launch {
+            db.collection("usuarios")
+                .whereEqualTo("email", email)
+                .get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        name = document.getString("nombre").toString()
+                        contrasena = document.getString("contraseña").toString()
+                        stringList.add(name)
+                    }
+                }
+                .addOnFailureListener { exception ->
+
+                }
+
+        }
+        for (item in stringList) {
+            if (item.toString().equals(stringList.toString()))
+                Toast.makeText(this, "El nombre de usuario ya existe", Toast.LENGTH_SHORT).show()
             return false
         }
         return true
