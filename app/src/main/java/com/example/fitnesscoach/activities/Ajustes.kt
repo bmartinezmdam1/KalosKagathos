5b0850d7bce49789bbeec18c7e7b6937da6efe85
bmartinezmdam1 <bmartinezmdam1@gmail.com>
2024-06-03 18:15:49 +0200

    intento de ajustes

---
+++ app/src/main/java/com/example/fitnesscoach/activities/Ajustes.kt
@@ -0,0 +1,87 @@
+package com.example.fitnesscoach
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class Ajustes : Fragment() {
+    private lateinit var boton1: Button
+    private lateinit var boton2: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.ajustes, container, false)
+        boton1 = view.findViewById(R.id.cambiarFoto)
+        boton2 = view.findViewById(R.id.ActivarNotificaciones)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        context?.let { createNotificationChannel(it) }
+        boton1.setOnClickListener { btnCambiarFoto(it) }
+        boton2.setOnClickListener { btnActivarNotificaciones(it) }
+    }
+
+    fun btnCambiarFoto(view: View) {
+        // Implementar lógica para cambiar la foto
+    }
+
+    fun btnActivarNotificaciones(view: View) {
+        sendNotification(requireContext())
+    }
+
+    private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "mi_canal_id"
+            val channelName = "Mi Canal"
+            val channelDescription = "Descripción de mi canal"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(context: Context) {
+        val channelId = "mi_canal_id"
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.bellnot) // Asegúrate de que tienes un icono válido
+            .setContentTitle("Es hora de entrenar!")
+            .setContentText("Es hora de entrenar!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(context)) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            notify(1, builder.build())
+        }
+    }
+}
--- app/src/main/java/com/example/fitnesscoach/activities/InicioActivity.kt
+++ app/src/main/java/com/example/fitnesscoach/activities/InicioActivity.kt
@@ -1,14 +1,17 @@
 package com.example.fitnesscoach.activities
 
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fitnesscoach.Ajustes
 import com.example.fitnesscoach.R
+
 
 class InicioActivity : AppCompatActivity() {
     private lateinit var principianteBoton: Button
@@ -20,31 +23,41 @@ class InicioActivity : AppCompatActivity() {
     private lateinit var imagenAvanzado: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        try {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.inicio)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio)
 
-            principianteBoton = findViewById(R.id.botonPrincipiante)
-            intermedioBoton = findViewById(R.id.botonIntermedio)
-            avanzadoBoton = findViewById(R.id.botonAvanzado)
-            textView = findViewById(R.id.textView)
-            imagenPrincipiante = findViewById(R.id.imagenPrincipiante)
-            imagenIntermedio = findViewById(R.id.imagenIntermedio)
-            imagenAvanzado = findViewById(R.id.imagenAvanzado)
+        principianteBoton = findViewById(R.id.botonPrincipiante)
+        intermedioBoton = findViewById(R.id.botonIntermedio)
+        avanzadoBoton = findViewById(R.id.botonAvanzado)
+        textView = findViewById(R.id.textView)
+        imagenPrincipiante = findViewById(R.id.imagenPrincipiante)
+        imagenIntermedio = findViewById(R.id.imagenIntermedio)
+        imagenAvanzado = findViewById(R.id.imagenAvanzado)
 
-            principianteBoton.setOnClickListener {
-                showFragment()
+        // Example button to show fragment
+        principianteBoton.setOnClickListener {
+            showFragment()
+        }
+        intermedioBoton.setOnClickListener {
+            showFragment()
+        }
+        avanzadoBoton.setOnClickListener {
+            showFragment()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main_drawer, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.nav_ajustes -> {
+                showAjustesFragment()
+                true
             }
-
-            intermedioBoton.setOnClickListener {
-                showFragment()
-            }
-
-            avanzadoBoton.setOnClickListener {
-                showFragment()
-            }
-        } catch (e: Exception) {
-            Log.e("Error:", e.toString())
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
@@ -55,6 +68,26 @@ class InicioActivity : AppCompatActivity() {
             .commit()
 
         // Ocultar las vistas del Activity
+        hideActivityViews()
+
+        // Mostrar el contenedor del Fragment
+        findViewById<FrameLayout>(R.id.fragment_container).visibility = View.VISIBLE
+    }
+
+    private fun showAjustesFragment() {
+        val fragment = Ajustes()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+
+        // Ocultar las vistas del Activity
+        hideActivityViews()
+
+        // Mostrar el contenedor del Fragment
+        findViewById<FrameLayout>(R.id.fragment_container).visibility = View.VISIBLE
+    }
+
+    private fun hideActivityViews() {
         textView.visibility = View.GONE
         imagenPrincipiante.visibility = View.GONE
         imagenIntermedio.visibility = View.GONE
@@ -62,8 +95,5 @@ class InicioActivity : AppCompatActivity() {
         principianteBoton.visibility = View.GONE
         intermedioBoton.visibility = View.GONE
         avanzadoBoton.visibility = View.GONE
-
-        // Mostrar el contenedor del Fragment
-        findViewById<FrameLayout>(R.id.fragment_container).visibility = View.VISIBLE
     }
 }
